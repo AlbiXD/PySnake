4c3ff53eb04367232b82a6111630c588b1b2fab2
albizhaku15@gmail.com <albizhaku15@gmail.com>
2025-01-30 22:37:31 -0500

    Fixed grid for apple

--- snake.py
+++ snake.py
@@ -8,11 +8,12 @@ gameWindow.geometry("500x500")
 canvas = tk.Canvas(gameWindow, width=500, height=500, bg="white")
 canvas.pack()
 
+
 SCALE = 25
-HEAD = "U"
+HEAD = "F"
 
 isApple = False;
-
+previousIndex = 0
 grid = ["E"] * 400
 
 # Create GRID
@@ -27,7 +28,10 @@ for x in range(SCALE):
 
 
 # x1 y1 x2 y2
+
 rect = canvas.create_rectangle(0, 0, SCALE, SCALE, fill="green", outline="black")
+
+
 apple = 0
 # Movement Functions
 def move_left(event):
@@ -46,26 +50,37 @@ def move_down(event):
     global HEAD
     HEAD = "D"
 
+def print_grid(event):
+    for i in range(len(grid)):  # Remove +1
+        print(grid[i], end=" ")
+        if (i + 1) % 20 == 0:  # Adjust condition
+            print()  # Newline after every 20 elements
+    print("-----------------------------------------")
+
 gameWindow.bind("<Left>", move_left)
 gameWindow.bind("<Right>", move_right)
 gameWindow.bind("<Up>", move_up)
 gameWindow.bind("<Down>", move_down)
+gameWindow.bind("r", print_grid)
 
-canvas.move(rect, 50, 50)
+sX = rndm.randint(1,18) 
+sY = rndm.randint(3,16)
+canvas.move(rect, SCALE*sX, SCALE*sY)
+
+x1, y1, x2, y2 = canvas.coords(rect)
+prevIndex = int(((x2/25)*(y2/25))-1)
 
 def drawApple():
     global apple
     global isApple
     if isApple == False:
         apple = canvas.create_rectangle(0,0,SCALE, SCALE, fill ="red", outline="black")
-        randomX = rndm.randint(0,19) 
-        randomY = rndm.randint(0,19) 
-        canvas.move(apple, SCALE*randomX,SCALE*randomY)
+        row = rndm.randint(0,19) 
+        col = rndm.randint(0,19)
+        print(row+1, col+1)
+        canvas.move(apple, SCALE*row,SCALE*col)
         isApple = True
-        grid[randomX*randomY] = "A"
-        print(randomX)
-        print(randomY)
-        print(randomX*randomY)
+        grid[(col*20) + (row)] = "A"
 
 
 
@@ -75,9 +90,11 @@ def Collision():
 # Continuous Movement
 def gameLogic():
     global HEAD
-
+    global prevIndex
+    
     drawApple()
 
+    #LETS MOVE THE DAMN SNAKE
     if HEAD == "U":
         canvas.move(rect, 0, -SCALE)
     elif HEAD == "D":
@@ -86,13 +103,32 @@ def gameLogic():
         canvas.move(rect, -SCALE, 0)
     elif HEAD == "R":
         canvas.move(rect, SCALE, 0)
-    #Detect collision
+
+    
     x1, y1, x2, y2 = canvas.coords(rect)
 
 
+    
+    index = int(((x2/25)*(y2/25))-1)
 
-    gameWindow.after(200, gameLogic) 
+
+    x = (x2/25)-1
+    y = (y2/25)-1
+    
+    if(index < 0 or x > 19 or y > 19):
+        gameWindow.destroy()
+        return
+    
+    grid[index] = "S"
+    print("Current Index: ", index)
+    print("Previous Index: ", prevIndex)
+
+    prevIndex = index;
+    gameWindow.after(20000, gameLogic) 
 
 
 gameLogic()
 gameWindow.mainloop()
+
+
+
